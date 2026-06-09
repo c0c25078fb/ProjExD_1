5efb0b96e410263927846e2e9abbe5d2b470324c
Ren <c0c25078fb@edu.teu.ac.jp>
2026-06-09 15:34:15 +0900

    練習３と４：こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kt_img = pg.image.load("fig/3.png") #練習３：こうかとんSurfaceの作成
+    kt_img = pg.transform.flip(kt_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kt_img, [300, 200]) #練習４：こうかとんSurfaceを描画する
         pg.display.update()
         tmr += 1        
         clock.tick(10)
