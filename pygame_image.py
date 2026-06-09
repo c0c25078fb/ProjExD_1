43645508f9760885f1db12e2b89d36b99a026344
Ren <c0c25078fb@edu.teu.ac.jp>
2026-06-09 15:42:55 +0900

    練習５と６：背景画像が右から左に流れるようになった

--- pygame_image.py
+++ pygame_image.py
@@ -16,12 +16,14 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+            
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
         screen.blit(kt_img, [300, 200]) #練習４：こうかとんSurfaceを描画する
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
